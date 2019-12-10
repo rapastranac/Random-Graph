646409792d5cf32e1d5a6bf3bab70063fe3bfa4c
Andres P <rapastranac@gmail.com>
2019-12-09 20:56:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+# load libraries
+import numpy as np
+from numpy.random import seed
+from numpy.random import rand
+#from rdoclient_py3 import RandomOrgClient
+
+#key1 = 'b9473ffe-e294-4ea8-ad67-ccce0fb86ef2'
+#seed = RandomOrgClient(key1)
+#r = seed.generate_decimal_fractions(2, 4)
+
+
+def getrndgraph(n, p):
+    g = np.zeros([n, n], dtype=int)
+    seed(1)
+
+    for i in range(0, n):
+        for j in range(i + 1, n):
+            r = rand()
+            if r <= p:
+                g[i][j] = 1
+                g[j][i] = 1
+    print(g)
+    return g
+
+
+def printfile(g, n):
+    file = open("graph.txt", "w")
+    for i in range(0, n):
+        file.write("%s\t" % i)
+        for j in range(0, n):
+            if g[i][j] == 1:
+                file.write("%s\t" % j)
+        file.write(str('\n'))
+
+
+def main():
+    n = 70
+    #p = 4 / n
+    p = 0.04
+    g = getrndgraph(n, p)
+    printfile(g, n)
+
+
+main()
